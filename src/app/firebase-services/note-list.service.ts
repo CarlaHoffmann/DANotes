5fc5a35556e1447d2437cc78256e634e99ae6374
CarlaHoffmann <carlahoffmann@gmx.de>
2025-04-28 14:13:47 +0200

    added addNote

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
-import { Note } from '../interfaces/note.interface'
+import { Note } from '../interfaces/note.interface';
 import { AsyncPipe } from '@angular/common';
-import { Firestore, collectionData, collection, doc, onSnapshot } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, doc, onSnapshot, addDoc } from '@angular/fire/firestore';
 import { Observable } from "rxjs";
 
 // interface Item {
@@ -33,37 +33,19 @@ export class NoteListService {
   constructor() {
     this.unsubNotes = this.subNotesList();
     this.unsubTrash = this.subTrashList();
-
-
-    // this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
-    //   list.forEach(element => {
-    //     console.log(element.id);
-    //     console.log(this.setNoteObject(element.data(), element.id));
-    //   });
-    // });
-
-    // this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "Q7a2jVJqlj04WA270x0t"), (element) => {
-    //   console.log(element);
-    // });
-
-    // this.item$ = collectionData(this.getNotesRef()); // Erstellt Observable, liefert Werte async, oft mehrfach
-    // // subscribe abonniert den Datenstrom und führt die Funktion 
-    // // immer dann aus, wenn sich die Daten ändern
-    // this.item = this.item$.subscribe((list) => { 
-    //   list.forEach(element => {
-    //     console.log(element);
-    //   });
-    // })
-
   }
 
 
+  async addNote(item: Note) {
+    await addDoc(this.getNotesRef(), item).catch(
+      (err) => { console.error(err)}
+    ).then(
+      (docRef) => { console.log("Document written with ID: ", docRef?.id) }
+    )
+    
+  }
+
   ngOnDestroy() {
-    // unsubscribe beendet das Abo, damit keine Daten mehr fließen
-    // this.item.unsubscribe(); 
-    // this.unsubList(); // beendet Abo (wie unsubscribe)
-    // this.unsubSingle(); // beendet Abo
-    this.unsubNotes();
     this.unsubTrash();
   }
 
@@ -85,7 +67,15 @@ export class NoteListService {
     });
   }
 
-
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || "note",
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false,
+    }
+  }
 
   getNotesRef() {
     return collection(this.firestore, 'notes');
@@ -97,16 +87,5 @@ export class NoteListService {
 
   getSingleDocRef(collId: string, docId: string) {
     return doc(collection(this.firestore, collId), docId)
-  }
-
-
-  setNoteObject(obj: any, id: string): Note {
-    return {
-      id: id,
-      type: obj.type || "note",
-      title: obj.title || '',
-      content: obj.content || '',
-      marked: obj.marked || false,
-    }
   }
 }
